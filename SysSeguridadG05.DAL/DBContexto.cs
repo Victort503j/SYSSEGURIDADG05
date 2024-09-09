065dc9893889e3be0677b46961377d364c45e84f
Victort503j <victor.romerorc@gmail.com>
2024-09-09 16:56:46 -0700

    first commit

---
+++ SysSeguridadG05.DAL/DBContexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SysSeguridadG05.EN;
+
+namespace SysSeguridadG05.DAL
+{
+    public class DBContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseMySql("Server=localhost;Database=DBSeguridadG05;User=root;Password=password;",
+            optionsBuilder.UseSqlServer(@"workstation id = DbSeguridadG05.mssql.somee.com; packet size = 4096; user id = victor2005_SQLLogin_1; pwd = zk4o1kgw5h; data source = DbSeguridadG05.mssql.somee.com; persist security info = False; initial catalog = DbSeguridadG05; TrustServerCertificate = True");
+                //new MySqlServerVersion(new Version(8, 0, 0)));
+        }
+
+    }
+}
---
+++ SysSeguridadG05.WebApi/Controllers/RolController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SysSeguridadG05.BL;
+using SysSeguridadG05.EN;
+using System.Text.Json;
+
+namespace SysSeguridadG05.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private RolBL rolBl = new RolBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Rol>> Get()
+        {
+            return await rolBl.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Rol> Get(int id)
+        {
+            Rol rol = new Rol();
+            rol.Id = id;
+            return await rolBl.ObtenerPorIdAsync(rol);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] object pRol)
+        {
+            try
+            {
+                var option = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                string strRol = JsonSerializer.Serialize(pRol);
+                Rol rol = JsonSerializer.Deserialize<Rol>(strRol, option);
+                await rolBl.CrearAsync(rol);
+                return Ok();
+            }
+            catch (Exception Ex)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] object pRol)
+        {
+            try
+            {
+                var option = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                string strRol = JsonSerializer.Serialize(pRol);
+                Rol rol = JsonSerializer.Deserialize<Rol>(strRol, option);
+                if(rol.Id == id)
+                {
+                    await rolBl.ModificarAsync(rol);
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                await rolBl.DeleteAsync(new Rol { Id = id});
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+               return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Buscar")]
+        public async Task<List<Rol>> Buscar([FromBody] object pRol)
+        {
+            try
+            {
+                var option = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var strRol = JsonSerializer.Serialize(pRol);
+                Rol rol = JsonSerializer.Deserialize<Rol>(strRol, option);
+                return await rolBl.BuscarAsync(rol);
+            }
+            catch (Exception ex)
+            {
+                return new List<Rol>();
+            }
+        }
+    }
+}
